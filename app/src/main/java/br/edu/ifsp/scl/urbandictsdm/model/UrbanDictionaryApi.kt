c547395977bf8511d54315c908c3b09d643efab8
Yuri <yurihp01@hotmail.com>
2020-05-06 17:01:44 -0300

    inserindo no projeto

---
+++ app/src/main/java/br/edu/ifsp/scl/urbandictsdm/model/UrbanDictionaryApi.kt
@@ -0,0 +1,10 @@
+package br.edu.ifsp.scl.urbandictsdm.model
+
+object UrbanDictionaryApi {
+    val URL_BASE = "https://mashape-community-urban-dictionary.p.rapidapi.com/"
+    val END_POINT = "define?term=" // Não é bem um endpoint, mas...
+    val X_RAPIDAPI_KEY_FIELD = "x-rapidapi-key"
+    val X_RAPIDAPI_KEY_VALUE = "da0f5bfc41msh56785302178ec36p1683c3jsnd80cdbaee9df"
+    val X_RAPIDAPI_HOST_FIELD = "x-rapidapi-host"
+    val X_RAPIDAPI_HOST_VALUE = "mashape-community-urban-dictionary.p.rapidapi.com"
+}
